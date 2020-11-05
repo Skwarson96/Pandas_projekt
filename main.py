07b730e60a6a73c70bf1b012ac8c178ac9197162
Maciej <maciej.skwara2@gmail.com>
2020-11-05 20:22:29 +0100

    Zad 1

---
+++ main.py
@@ -0,0 +1,64 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+import numpy as np
+import os
+import glob
+from pandas import DataFrame
+
+
+
+def zad1():
+    os.chdir(r'names')
+    list_of_txt_files = glob.glob('*.txt')
+    list_of_txt_files.sort()
+    columns = []
+    for name in list_of_txt_files:
+        columns.append(str(name[3:7]) + " Name")
+        columns.append(str(name[3:7]) + " Sex")
+        columns.append(str(name[3:7]) + " Number")
+    # print(columns)
+    columns2 = ["Name", "Sex", "Number"]
+    indexes = []
+    for name_of_file in list_of_txt_files:
+        df1 = pd.read_csv(name_of_file, sep=',', header=None)
+        indexes.append(len(df1[0]))
+    # print(max(indexes))
+
+    # print(list_of_txt_files)
+    df = pd.DataFrame(columns=columns, index=range(0, max(indexes)))
+
+
+    index = 0
+    for name_of_file in list_of_txt_files:
+        df1 = pd.read_csv(name_of_file, sep=',', header=None)
+
+        df[columns[index]] = df1[0]
+        df[columns[index + 1]] = df1[1]
+        df[columns[index + 2]] = df1[2]
+
+        index = index + 3
+        print(index)
+
+    print(df)
+
+
+
+
+
+
+def main():
+    zad1()
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
+
+
+
