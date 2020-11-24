0f1b982dbb69f874e6c6c157439fda1ad4afd7d7
Maciej <maciej.skwara2@gmail.com>
2020-11-24 10:06:48 +0100

    ZadZad2_2 & Zad3_2 and delete old solutions

--- main.py
+++ main.py
@@ -8,40 +8,6 @@ from pandas import DataFrame
 import operator
 import math
 
-def zad1():
-    ''' Wczytaj dane ze wszystkich plików do pojedynczej tablicy (używając Pandas). '''
-    os.chdir(r'names')
-    list_of_txt_files = glob.glob('*.txt')
-    list_of_txt_files.sort()
-    columns = []
-    for name in list_of_txt_files:
-        columns.append(str(name[3:7]) + " Name")
-        columns.append(str(name[3:7]) + " Sex")
-        columns.append(str(name[3:7]) + " Number")
-    # print(columns)
-    columns2 = ["Name", "Sex", "Number"]
-    indexes = []
-    for name_of_file in list_of_txt_files:
-        df1 = pd.read_csv(name_of_file, sep=',', header=None)
-        indexes.append(len(df1[0]))
-    # print(max(indexes))
-
-    # print(list_of_txt_files)
-    df = pd.DataFrame(columns=columns, index=range(0, max(indexes)))
-
-
-    index = 0
-    for name_of_file in list_of_txt_files:
-        df1 = pd.read_csv(name_of_file, sep=',', header=None)
-
-        df[columns[index]] = df1[0]
-        df[columns[index + 1]] = df1[1]
-        df[columns[index + 2]] = df1[2]
-
-        index = index + 3
-        print(index)
-
-    return df
 
 def zad1_2():
     ''' Wczytaj dane ze wszystkich plików do pojedynczej tablicy (używając Pandas). '''
@@ -64,73 +30,31 @@ def zad1_2():
     return data2
     # return df
 
-def zad2(data):
+def zad2_2(data):
     ''' Określi ile różnych (unikalnych) imion zostało nadanych w tym czasie. '''
-    print(data)
-    unique_names = []
-    list_of_years = list(range(1880, 2020))
-    print(list_of_years)
+    data2 = data.groupby(level=1).sum()
+    # print(data2.shape)
+    print('Ilosc nadanych unikalnych imion w pelnym okresie:',data2.shape[0])
 
-    for column in data.columns:
-        if column[5:9] == "Name":
-            for name in data[column]:
-                if name not in unique_names:
-                    unique_names.append(name)
-                    print(name)
-    print(len(unique_names))
-    # 99445
-    # !!!!!!!!!!!!!!!
-    #     print(data.groupby(level=1).sum())
-    #
-
-def zad3(data):
+def zad3_2(data):
     ''' Określi ile różnych (unikalnych) imion zostało nadanych w tym czasie rozróżniając imiona męskie i żeńskie. '''
-    print(data)
-    unique_female_names = []
-    unique_male_names = []
-    list_of_years = list(range(1880, 2020))
+    # print(data)
+    data_female = data.iloc[:,0]
+    data_female = data_female.dropna()
+    data_female = data_female.groupby(level=1).sum()
+    # print(data_female)
 
-    for year in list_of_years:
-        for index in data.index:
-            # print(data.loc[index, str(year) + " Name"], data.loc[index, str(year) + " Sex"])
+    data_male = data.iloc[:,1]
+    data_male = data_male.dropna()
+    data_male = data_male.groupby(level=1).sum()
+    # print(data_male)
 
-            if data.loc[index, str(year) + " Sex"] == 'F' and data.loc[index, str(year) + " Name"] not in unique_female_names:
-                unique_female_names.append(data.loc[index, str(year) + " Name"])
-                print(data.loc[index, str(year) + " Name"], data.loc[index, str(year) + " Sex"])
-
-            if data.loc[index, str(year) + " Sex"] == 'M' and data.loc[index, str(year) + " Name"] not in unique_male_names:
-                unique_male_names.append(data.loc[index, str(year) + " Name"])
-                print(data.loc[index, str(year) + " Name"], data.loc[index, str(year) + " Sex"])
-        # print()
-
-    print(len(unique_female_names))
-    # 68332
-    print(len(unique_male_names))
-    # 42054
-
-def zad4(data):
-    ''' Stwórz nowe kolumny frequency_male i frequency_female i określ popularność każdego z imion w danym każdym
-    roku dzieląc liczbę razy, kiedy imię zostało nadane przez całkowita liczbę urodzeń dla danej płci.  '''
-    list_of_years = list(range(1880, 2020))
-
-    data2 = data.groupby(['1880 Sex']).sum()
-    for year in list_of_years:
-        data[str(year) + ' frequency_female'] = None
-        data[str(year) + ' frequency_male'] = None
-        sum_of_female_names = data2.loc['F', str(year) + ' Number']
-        sum_of_male_names = data2.loc['M', str(year) + ' Number']
-        print(str(year), sum_of_female_names)
-        print(str(year), sum_of_male_names)
-
-        data[str(year) + ' frequency_female'] = data[str(year) + ' Number']/sum_of_female_names
-        data[str(year) + ' frequency_male'] = data[str(year) + ' Number']/sum_of_male_names
-
-    print(data[['1880 Name', '1880 Sex', '1880 Number', '1880 frequency_female', '1880 frequency_male']])
-
-    # 1880 90994.0
-    # 1880 110490.0
+    print('Ilosc unikalnych kobiecych imion:', data_female.shape[0])
+    print('Ilosc unikalnych meskich imion:', data_male.shape[0])
 
 def zad4_2(data):
+    ''' Stwórz nowe kolumny frequency_male i frequency_female i określ popularność każdego z imion w danym każdym
+    roku dzieląc liczbę razy, kiedy imię zostało nadane przez całkowita liczbę urodzeń dla danej płci.  '''
     # print(data)
     data2 = data.groupby(level=0).sum()
     # print(data2)
@@ -774,7 +698,6 @@ def zad14(birth_data, death_data):
     ax.set_title("Zadanie 14, 15")
     plt.show()
 
-
 def zad15(birth_data, death_data):
     '''
     Na wykresie z pkt 14 wyznacz współczynnik przeżywalności dzieci w pierwszych 5 latach życia
@@ -819,7 +742,9 @@ def main():
     # data = zad1()
     birth_data = zad1_2()
     # zad2(data)
+    # zad2_2(birth_data)
     # zad3(data)
+    zad3_2(birth_data)
     # zad4(data)
     # zad4_2(birth_data)
     # zad5_2(birth_data)
@@ -829,9 +754,9 @@ def main():
     # zad9_2(birth_data)
     # zad10_2(birth_data)
     # zad11_2(birth_data)
-    death_data = zad12()
+    # death_data = zad12()
     # zad13(birth_data, death_data)
-    zad14(birth_data, death_data)
+    # zad14(birth_data, death_data)
     # zad15(birth_data, death_data)
 
 
